d86decb12e5e1cae084729acc92af3641cec446b
Thilo Schwarz <thilo.schwarz@googlemail.com>
2019-05-05 14:44:29 +0200

    added thymeleaf

---
+++ src/main/java/codes/thischwa/bacoma/ui/controller/ThymeleafController.java
@@ -0,0 +1,18 @@
+package codes.thischwa.bacoma.ui.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ThymeleafController {
+    @Value("${spring.application.name}")
+    String appName;
+ 
+    @GetMapping("/thymeleaf")
+    public String homePage(Model model) {
+        model.addAttribute("appName", appName);
+        return "home";
+    }
+}
